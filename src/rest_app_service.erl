02dfc4ebec9e08c78b668af349f77946153dabdb
poornima prasad <poornima.prasad@redbus.com>
2025-02-14 06:21:14 +0530

    [+] Required files to build an app

---
+++ src/rest_app_service.erl
@@ -0,0 +1,13 @@
+%%%-------------------------------------------------------------------
+%%% @author poornima.prasad
+%%% @copyright (C) 2025, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 14. Feb 2025 6:14 am
+%%%-------------------------------------------------------------------
+-module(rest_app_service).
+-author("poornima.prasad").
+
+%% API
+-export([]).
